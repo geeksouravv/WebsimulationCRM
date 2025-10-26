e19bf3dbeaa6b4f0fcbe1463ed37fcfa61f13193
HexZError <souravmavi70@gmail.com>
2025-10-27 00:03:46 +0530

    Login Page UI Code Update

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebsimulationCRM.Controllers
+{
+    public class AccountController : Controller
+    {
+        [Route("/")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
