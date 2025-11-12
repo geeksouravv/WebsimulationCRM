9e1b60fcef6ff6b3bb1546133c1df0af62e7308e
Piyush <dpiyush28@gmail.com>
2025-11-13 00:46:47 +0530

    LoginCode Update

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,13 +1,66 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebsimulationCRM.CORE.Domain.IdentityEntities;
+using WebsimulationCRM.CORE.DTO;
 
-namespace WebsimulationCRM.Controllers
+namespace WebsimulationCRM.UI.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly Microsoft.AspNetCore.Identity.UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AccountController(Microsoft.AspNetCore.Identity.UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
         [Route("/")]
         public IActionResult Login()
         {
             return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterDTO registerDTO)
+        {
+            if(ModelState.IsValid == false)
+            {
+                ViewBag.Errors = ModelState.Values.SelectMany(temp =>  temp.Errors).Select(temp => temp.ErrorMessage);
+
+                return View(registerDTO);
+            }
+
+            ApplicationUser user = new ApplicationUser()
+            {
+                Email = registerDTO.Email,
+                PhoneNumber = registerDTO.PhoneNumber,
+                UserName = registerDTO.Email,
+                EmployeeName = registerDTO.UserName,
+            };
+
+           Microsoft.AspNetCore.Identity.IdentityResult result = await _userManager.CreateAsync(user,registerDTO.Password);
+            if(result.Succeeded)
+            {
+                await _signInManager.SignInAsync(user, isPersistent: false);
+
+                return RedirectToAction("index", "Home");
+            }
+            else
+            {
+                foreach(IdentityError error in result.Errors)
+                {
+                    ModelState.AddModelError("Register", error.Description);
+                }
+
+                return View(registerDTO);
+            }
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,15 +1,23 @@
-using Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using WebsimulationCRM.CORE.Domain.IdentityEntities;
+using WebsimulationCRM.Infrastructure.DBContext;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
 });
+builder.Services.AddIdentity<ApplicationUser, ApplicationRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders()
+    .AddUserStore<UserStore<ApplicationUser , ApplicationRole,ApplicationDbContext,int>>()
+    .AddRoleStore<RoleStore<ApplicationRole,ApplicationDbContext,int>>();
+// Add services to the container.
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
