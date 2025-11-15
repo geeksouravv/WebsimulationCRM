dd22ce4e472025e5a03b5a57a959a0546dc83536
Piyush <dpiyush28@gmail.com>
2025-11-15 12:24:06 +0530

    Code-Update #1001

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -6,6 +6,7 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using WebsimulationCRM.CORE.Domain.IdentityEntities;
 using WebsimulationCRM.CORE.DTO;
+using WebsimulationCRM.CORE.Enums;
 
 namespace WebsimulationCRM.UI.Controllers
 {
@@ -14,11 +15,13 @@ namespace WebsimulationCRM.UI.Controllers
     {
         private readonly Microsoft.AspNetCore.Identity.UserManager<ApplicationUser> _userManager;
         private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly Microsoft.AspNetCore.Identity.RoleManager<ApplicationRole> _roleManager;
 
-        public AccountController(Microsoft.AspNetCore.Identity.UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        public AccountController(Microsoft.AspNetCore.Identity.UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, Microsoft.AspNetCore.Identity.RoleManager<ApplicationRole> roleManager)
         {
             _userManager = userManager;
             _signInManager = signInManager;
+            _roleManager = roleManager;
         }
 
         [AllowAnonymous]
@@ -87,13 +90,48 @@ namespace WebsimulationCRM.UI.Controllers
                 PhoneNumber = registerDTO.PhoneNumber,
                 UserName = registerDTO.Email,
                 EmployeeName = registerDTO.UserName,
-                Role = registerDTO.Role,
+                Role = registerDTO.Role.ToString(),
                 TeamName = registerDTO.TeamName
             };
 
            Microsoft.AspNetCore.Identity.IdentityResult result = await _userManager.CreateAsync(user,registerDTO.Password);
             if(result.Succeeded)
             {
+                //create role
+                if(registerDTO.Role == UserTypeOptions.SuperAdmin)
+                {
+                    if(await _roleManager.FindByNameAsync(UserTypeOptions.SuperAdmin.ToString()) is null)
+                    {
+                        ApplicationRole applicationRole = new ApplicationRole() { Name = UserTypeOptions.SuperAdmin.ToString() };
+                        await _roleManager.CreateAsync(applicationRole);
+                    }
+
+                    //add role
+                   await _userManager.AddToRoleAsync(user,UserTypeOptions.SuperAdmin.ToString());
+                }
+                else if(registerDTO.Role == UserTypeOptions.Manager)
+                {
+                    if(await _roleManager.FindByNameAsync(UserTypeOptions.Manager.ToString()) is null)
+                    {
+                        ApplicationRole applicationRole = new ApplicationRole() {Name = UserTypeOptions.Manager.ToString() };
+                       await _roleManager.CreateAsync(applicationRole);
+                    }
+
+                    await _userManager.AddToRoleAsync(user, UserTypeOptions.Manager.ToString());
+                }
+
+                else
+                {
+                    if(await _roleManager.FindByNameAsync(UserTypeOptions.Employee.ToString()) is null)
+                     {
+                        ApplicationRole applicationRole = new ApplicationRole() { Name = UserTypeOptions.Employee.ToString() };
+                        await _roleManager.CreateAsync(applicationRole);
+                    }
+                    
+
+                    await _userManager.AddToRoleAsync(user, UserTypeOptions.Employee.ToString());
+                }
+
                 await _userManager.AddClaimAsync(user, new Claim("EmployeeName", user.EmployeeName));
 
                 await _signInManager.SignInAsync(user, isPersistent: false);
